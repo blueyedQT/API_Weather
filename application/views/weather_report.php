bf0594866c8f9c51bebc91ba485ef1dee9ad48e0
Katrina Sanford <blueyed_qt@hotmail.com>
2014-12-05 15:02:02 -0800

    Functional, just need to add city name to results

--- application/views/weather_report.php
+++ application/views/weather_report.php
@@ -6,25 +6,50 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
-			alert('js is working');
-		})
+			$("form").submit(function() {
+				$.get(
+					$(this).attr('action')+"?callback=?",
+					$(this).serialize(), 
+					function(output) {
+						console.log(output);
+						// city = '';
+						// if(output.data.request[0].query == '98055') {
+						// 	city = 'Renton, WA';
+						// } elseif(output.data.request[0].query == '98102') {
+						// 	city == 'Las Vegas, NV';
+						// }
+						tempF = output.data.current_condition[0].temp_F;
+						tempC = output.data.current_condition[0].temp_C;
+						wind = output.data.current_condition[0].windspeedMiles;
+						description = output.data.current_condition[0].weatherDesc[0].value;
+						src = output.data.current_condition[0].weatherIconUrl[0].value;
+						$('#forcast').html("<h2>Weather for "+"</h2>"+
+							"<p>Current Temp F: "+tempF+" degrees</p>"+
+							"<p>Current Temp C: "+tempC+" degrees</p>"+
+							"<p>Current Widnspeed: "+wind+" MPH</p>"+
+							"<p>Weather Description: "+description+"</p>"+
+							"<img alt='weather' src='"+src+"'' />"
+							);
+					}, "json"
+				);
+				return false;
+			});
+		});
 	</script>
 </head>
 <body>
 	<h1>The CodingDojo Weather Report!</h1>
-	<form action="http://api.worldweatheronline.com/free/v2/weather.ashx?callback=?" method="get">
+	<form action="http://api.worldweatheronline.com/free/v2/weather.ashx" method="get">
 		<select name="q">
 			<option>Select a City</option>
 			<option value="98055">Renton, WA</option>
 			<option value="89102">Las Vegas, NV</option>
 			<option value="84720">Cedar City, UT</option>
+			<input type="hidden" name="format" value="json">
+			<input type="hidden" name="key" value="fdb42bc9fbfaf5c150af110064a42">
 			<input type="submit" name="submit" value="Get Weather!">
 		</select>
 	</form>
-	<h2>Weather for (CITY)</h2>
-	<p>Current Temp F: (TEMP) degrees</p>
-	<p>Current Temp C: (TEMP) degrees</p>
-	<p>Current Widnspeed: (WINDSPEED) MPH</p>
-	<p>Weather Description: (WEATHER DESCRIPTION)</p>
+	<div id="forcast"></div>
 </body>
 </html>
